c8a1809da2a3e01e2d2d00e6842d9933dcab8c87
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-30 01:37:49 +0530

    Adding UC1

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,8 +1,23 @@
 package com;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to USER REGISTRATION PROBLEM");
+		Scanner s = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String firstName = s.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result = matcher.matches();
+        if(result == true)
+            System.out.println("First Name is Valid...");
+        else
+            System.out.println("First Name is not valid...");
 	}
 
 }
