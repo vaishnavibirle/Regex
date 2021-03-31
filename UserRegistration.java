732634fcbbcc8c64d6fe79ff5d3bde5f81cf3ae5
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-31 20:10:43 +0530

    Validating last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,6 +8,7 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to USER REGISTRATION PROBLEM");
 		Scanner s = new Scanner(System.in);
+		//validating first name
         System.out.println("Enter First Name: ");
         String firstName = s.next();
         String regex = "^[A-Z]{1}[a-z]{2,}$";
@@ -15,9 +16,19 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(firstName);
         boolean result = matcher.matches();
         if(result == true)
-            System.out.println("First Name is Valid...");
+            System.out.println("First Name is Valid");
         else
-            System.out.println("First Name is not valid...");
+            System.out.println("First Name is not valid");
+        
+        //validating last name
+        System.out.println("Enter Last Name: ");
+        String lastName = s.next();
+        matcher = pattern.matcher(lastName);
+        result = matcher.matches();
+        if(result == true)
+            System.out.println("Last Name is Valid");
+        else
+            System.out.println("Last Name is not valid");
+        
 	}
-
 }
